49de16d5c457e19b2b9d3530fbe98cf4a9fc7133
Dan <162982896+DanoneFraise@users.noreply.github.com>
2024-03-18 12:30:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,11 @@ import numpy as np
 voc = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSeARXO3MT92XWpg2IwyQOQ8Wi2upeEkqJvNJz5i3bRqHdJIrTchGBBclVu-3Jd1ohYKM4IxecgV64I/pub?output=csv')
 l = voc.shape[0]
 i = np.random.choice(range(l))
+#word_fr = voc['Définition'].values[i]
+#word_chi = voc['Hanzi'].values[i]
+#word_pin = voc['Pinyin'].values[i]
+#st.write(word_fr+" | "+word_chi+" | "+word_pin) 
+#st.button("refresh")
+
 word_fr = voc['Définition'].values[i]
-word_chi = voc['Hanzi'].values[i]
-word_pin = voc['Pinyin'].values[i]
-st.write(word_fr+" | "+word_chi+" | "+word_pin) 
-st.button("refresh")
+st.write(word_fr)
