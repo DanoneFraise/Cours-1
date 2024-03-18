5afefb77ab9d0f400b69c01f414cdc35da55329b
Dan <162982896+DanoneFraise@users.noreply.github.com>
2024-03-18 12:06:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import numphy as np
 voc = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSeARXO3MT92XWpg2IwyQOQ8Wi2upeEkqJvNJz5i3bRqHdJIrTchGBBclVu-3Jd1ohYKM4IxecgV64I/pub?output=csv')
-st.dataframe(voc)
 l = voc.shape[0]
 i = hp.random.choice(range(l))
 word_fr = voc['Définition'].values[i]
 word_chi = voc['Pinyin'].values[i]
 st.write(word_fr+" "+word_chi)
+st.button("refresh")
