e5435627e270376eb6f06b8b8c3fa71822b95cb7
Dan <162982896+DanoneFraise@users.noreply.github.com>
2024-03-18 11:38:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 name=st.text_input("Dan")
 st.write ("Hello " + name)
+
+import streamlit as st 
+import pandas as pd
+
+voc = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSeARXO3MT92XWpg2IwyQOQ8Wi2upeEkqJvNJz5i3bRqHdJIrTchGBBclVu-3Jd1ohYKM4IxecgV64I/pub?output=csv')
+st.dataframe(voc)
