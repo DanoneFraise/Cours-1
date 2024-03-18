5f72856fbc8ff69af47c3156c047eef637458c2c
Dan <162982896+DanoneFraise@users.noreply.github.com>
2024-03-18 12:08:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ voc = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSeARXO3MT92X
 l = voc.shape[0]
 i = np.random.choice(range(l))
 word_fr = voc['Définition'].values[i]
-word_chi = voc['Pinyin'].values[i]
+word_chi = voc['Hanzi'].values[i]
 st.write(word_fr+" "+word_chi)
 st.button("refresh")
